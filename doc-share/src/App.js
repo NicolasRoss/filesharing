bec1ea6df66bb3b45c70a5ff7c07493ab9a956c9
Noah Nichols <noahjacknichols@gmail.com>
2020-09-30 02:20:44 -0400

    added react website

---
+++ doc-share/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DocumentCard from './components/documentCard';
+
+function App() {
+  return (
+ 
+    <DocumentCard/>
+    
+  );
+}
+
+export default App;
---
+++ doc-share/src/components/documentCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import './documentCard.css';
+
+export default class documentCard extends React.Component{
+
+
+
+    render(){
+        return(
+            <Container>
+                <Row className="docContainer box-shadow">
+                    <Col xs={10}>
+                        <div className="uuidContainer">
+                            <div className="uuidContent">uuid goes here uuid goes here</div>
+                        </div>
+                    </Col>
+                    <Col xs={8}>
+                        <div>
+                            <div className="marginLeft">ExampleDocument.txt</div>
+                        </div>
+                    </Col>
+                    <Col xs={4}>
+                        <div>
+                            <div>September 21st, 2020</div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+
+}
