4d922efb64704f97d0bf38a1524a659efa913cff
Noah Nichols <noahjacknichols@gmail.com>
2020-09-29 21:11:52 -0400

    added post and users class

--- api/main.py
+++ api/main.py
@@ -33,9 +33,11 @@ class home(Resource):
 class Documents(Resource):
     
     def get(self):
+        parser.add_argument('key', type=str, help="key is needed to access documents")
         args = parser.parse_args()
+
         if(args['key'] is not None):
-            #run database retreival
+            
             cursor = conn.cursor()
             query = "SELECT * FROM documents WHERE uuid_id = %s"
             documents = cursor.execute(query, [args['key']])
@@ -46,10 +48,41 @@ class Documents(Resource):
             else:
                 return "invalid key", 400
         else:
-            return "no key offered", 400
+            return "no key submitted", 400
 
-    def put(self):
-        pass
+    def post(self):
+        parser.add_argument('text', type=str)
+        args = parser.parse_args()
+
+        if(args['text'] is not None):
+            cursor = conn.cursor()
+            query = "INSERT INTO documents (text) VALUES (%s)"
+            
+            d = cursor.execute(query, [args['text']])
+            print(args['text'])
+            print(d)
+            conn.commit()
+            cursor.close()
+            
+            return {"request": "success"}
+        else:
+            return "no text submitted", 400
+
+
+    '''
+    TODO: add a put message if we go that way
+    '''
+
+class Users(Resource):
+    def get(self):
+        parser.add_argument('user_id', type=int)
+        args = parser.parse_args()
+
+        if(args['user_id'] is not None):
+            
+            return {"request": "success"}
+        else:
+            return "no user_id submitted", 400
 
 
 
