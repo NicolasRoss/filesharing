99b991b359451e09692a7aa91ae937d9b8f1402c
Noah Nichols <noahjacknichols@gmail.com>
2020-09-30 12:39:11 -0400

    added more to frontend, rough navbar and user's page

--- doc-share/src/App.js
+++ doc-share/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DocumentCard from './components/documentCard';
-
+import DocCardContainer from './components/docCardContainer'
+import Navbar from './components/navbar';
 function App() {
   return (
- 
-    <DocumentCard/>
+    <div>
+      <Navbar/>
+      <DocCardContainer/>
+    </div>
     
   );
 }
--- doc-share/src/components/documentCard.jsx
+++ doc-share/src/components/documentCard.jsx
@@ -4,29 +4,50 @@ import './documentCard.css';
 
 export default class documentCard extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            uuid: '',
+            name: '',
+            date: ''
+        };
+    }
+
+    componentDidMount() {
+        //load data here i think
+    }
+
+
 
 
     render(){
         return(
-            <Container>
-                <Row className="docContainer box-shadow">
-                    <Col xs={10}>
-                        <div className="uuidContainer">
-                            <div className="uuidContent">uuid goes here uuid goes here</div>
-                        </div>
-                    </Col>
-                    <Col xs={8}>
-                        <div>
-                            <div className="docSubtitle marginLeft">ExampleDocument.txt</div>
-                        </div>
-                    </Col>
-                    <Col xs={4}>
-                        <div>
-                            <div className="docSubtitle">September 21st, 2020</div>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+            
+                <Container>
+                    <a href="#">
+                        <Row className="docContainer box-shadow">
+                            <Col xs={10}>
+                                
+                                <div className="uuidContainer">
+                                    <div className="uuidContent">5ec48ca8-029e-11eb-b3d9-04d4c40327b5</div>
+                                </div>
+                            </Col>
+                            <Col xs={8}>
+                                <div>
+                                    <div className="docSubtitle marginLeft">ExampleDocument.txt</div>
+                                </div>
+                            </Col>
+                            <Col xs={4}>
+                                <div>
+                                    <div className="docSubtitle">September 21st, 2020</div>
+                                </div>
+                                
+                            </Col>
+                            
+                        </Row>
+                    </a>
+                </Container>
+            
         );
     }
 
