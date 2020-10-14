96381c1ed60bccce90051aceb684f44e477aabbc
Noah Nichols <noahjacknichols@gmail.com>
2020-10-14 18:59:34 -0400

    changed ip

--- doc-share/src/components/api.jsx
+++ doc-share/src/components/api.jsx
@@ -1,7 +1,7 @@
 
 
 //for ec2 backend instance
-export const API = "http://3.131.95.162:5000"
+export const API = "http://18.217.253.29:5000"
 
 //for local development
 // export const API = "http://localhost:5000"
