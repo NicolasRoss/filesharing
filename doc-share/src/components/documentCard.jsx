c2c058fba83df4b706d75cf3e0f7364316cd8c78
Noah Nichols <noahjacknichols@gmail.com>
2020-10-17 23:23:01 -0400

    added account page and fixed searchbar

--- doc-share/src/components/documentCard.jsx
+++ doc-share/src/components/documentCard.jsx
@@ -179,15 +179,7 @@ class documentCard extends React.Component{
                         <i className="icon black fas fa-save" onClick={this.downloadClick}></i>
                         <i className="icon black fas fa-share-alt" onClick={this.shareClick}></i>
                         <i className="icon black fas fa-trash-alt" onClick={this.deleteClick}></i>
-                        {/* <button className="downloadButton noselect" onClick={this.downloadClick} download> Download File</button> */}
                     </Col>
-                    {/* <Col xs={{span: 4}}> */}
-                        
-                    {/* </Col> */}
-                    {/* <Col> */}
-                        
-                        {/* <button className="downloadButton noselect" onClick={this.deleteClick}>Delete file</button> */}
-                    {/* </Col> */}
                 </Row>
                 
             )
@@ -224,12 +216,6 @@ class documentCard extends React.Component{
                                         <div className="docSubtitle marginLeft noselect">{this.formatDate()}</div>
                                     </div>
                                 </Col>
-                                {/* <Col className="buffer" xs={5}>
-                                    <div>
-                                        <div className="docSubtitle floatRight noselect">{this.state.date}</div>
-                                    </div>
-                                    
-                                </Col> */}
                             </Row>
                         </div>
                             
