2973a53661807dfa4f8cb7d27dc4f189915184dc
Noah Nichols <noahjacknichols@gmail.com>
2020-10-19 19:44:25 -0400

    fixing api

--- doc-share/src/components/api.jsx
+++ doc-share/src/components/api.jsx
@@ -1,6 +1,7 @@
 //for ec2 backend instance
-export const API = "http://3.136.172.47:5000"
+// export const API = "http://3.136.172.47:5000"
 
 //for local development
+export const API = "http://localhost:5000";
 
 // export const API = "172.31.20.122:5000"
