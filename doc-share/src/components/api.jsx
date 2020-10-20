1c1ea9f75431849e600e0f6f79470c90c54f33a3
Noah Nichols <noahjacknichols@gmail.com>
2020-10-20 00:07:19 -0400

    fixed api for server

--- doc-share/src/components/api.jsx
+++ doc-share/src/components/api.jsx
@@ -1,8 +1,8 @@
 //for ec2 backend instance
-// export const API = "http://3.136.172.47:5000"
+export const API = "http://3.136.172.47:5000";
 
 //for local development
-export const API = "http://localhost:5000";
+// export const API = "http://localhost:5000";
 export const HOST = "http://3.136.172.47";
 
 // export const API = "172.31.20.122:5000"
