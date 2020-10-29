b4dfe8f38f1b3a5640b208c52b12bc68447c5fd6
NicolasRoss <Nick_ross1234@hotmail.com>
2020-10-29 16:16:38 -0400

    able to save files after editing them

--- doc-share/src/pages/DocViewer.jsx
+++ doc-share/src/pages/DocViewer.jsx
@@ -3,7 +3,6 @@ import { withRouter } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Navbar from "../components/navbar";
 import "../css/docViewer.css";
-import Cookies from "js-cookie";
 import { API } from "../components/api";
 
 class DocViewer extends React.Component {
@@ -11,16 +10,30 @@ class DocViewer extends React.Component {
     super(props);
     this.toHomePage = this.toHomePage.bind(this);
     this.checkFileExt = this.checkFileExt.bind(this);
+    this.handleFileSave = this.handleFileSave.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+
     this.state = {
       doc_info: [],
+      user_id: "",
+      value: "",
+      editable: false,
+      spellCheck: true,
+      blob: null,
     };
   }
 
   componentDidMount() {
     if (this?.props?.location?.state?.doc_info) {
-      this.setState({ doc_info: this.props.location.state.doc_info }, () => {
-        this.getFile();
-      });
+      this.setState(
+        {
+          doc_info: this.props.location.state.doc_info,
+          user_id: this.props.location.state.user_id,
+        },
+        () => {
+          this.getFile();
+        }
+      );
     }
   }
 
@@ -48,7 +61,7 @@ class DocViewer extends React.Component {
         return "code";
       } else if (ext[0].includes("zip")) {
         return "zip";
-      } else if (ext[0].includes("txt")) {
+      } else if (ext[0].includes("txt") || ext[0].includes("csv")) {
         return "text";
       }
     }
@@ -77,6 +90,7 @@ class DocViewer extends React.Component {
         })
         .then((blob) => {
           this.displayFile(blob);
+          // console.log(document.getElementById("save"))
         })
         .catch((error) => {
           alert(error.message);
@@ -85,26 +99,20 @@ class DocViewer extends React.Component {
   }
 
   displayFile = (blob) => {
+    this.setState({ blob: blob });
     var extension = this.checkFileExt();
     if (extension === "text") {
       var reader = new FileReader();
-      reader.addEventListener("loadend", function () {
-        const textContainer = document.getElementById("text");
-        textContainer.className = "docViewerContainer";
-        const textarea = document.createElement("textarea");
-        textarea.className = "textArea";
-        textarea.textContent = reader.result;
-        textContainer.append(textarea);
-
-        //add update button
-        const topBar = document.getElementById("topBar");
-        const updateButton = document.createElement("button");
-        updateButton.className = "backButton";
-        updateButton.textContent = "Submit";
-        topBar.append(updateButton);
+      reader.addEventListener("loadend", () => {
+        this.setState({ value: reader.result });
       });
       reader.readAsText(blob);
+      //add update button
+      this.setState({ editable: true, spellCheck: false });
     } else if (blob.type.startsWith("image/")) {
+      var textarea = document.getElementById("textarea");
+      textarea.parentNode.removeChild(textarea);
+
       const textContainer = document.getElementById("text");
       textContainer.className = "docViewerContainer";
 
@@ -114,26 +122,59 @@ class DocViewer extends React.Component {
       textContainer.append(img);
     } else if (extension === "code") {
       var reader = new FileReader();
-      reader.addEventListener("loadend", function () {
-        const textContainer = document.getElementById("text");
-        textContainer.className = "docViewerContainer";
-        const textarea = document.createElement("textarea");
-        textarea.className = "textArea";
-        textarea.spellcheck = false;
-        textarea.textContent = reader.result;
-        textContainer.append(textarea);
-
-        const topBar = document.getElementById("topBar");
-        const updateButton = document.createElement("button");
-        updateButton.className = "backButton";
-        updateButton.textContent = "Submit";
-        topBar.append(updateButton);
+      reader.addEventListener("loadend", () => {
+        this.setState({ value: reader.result });
       });
       reader.readAsText(blob);
+      //add update button
+      this.setState({ editable: true, spellCheck: false });
     } else {
       console.log(blob.type);
     }
   };
+
+  handleFileChange() {
+    this.setState({
+      value: document.getElementsByClassName("textArea")[0].value,
+    });
+  }
+
+  handleFileSave() {
+    const newContent = document.getElementsByClassName("textArea")[0]
+      .textContent;
+    var newBlob = new Blob([newContent], { type: this.state.blob.type });
+    var newFile = new File([newBlob], this.state.doc_info["document_name"]);
+    
+    if (newFile !== undefined) {
+      const data = new FormData();
+      data.append("file", newFile);
+      var url =
+        API +
+        "/documents?user=" +
+        this.state.user_id +
+        "&uuid=" +
+        this.state.doc_info["uuid_id"];
+
+      if (newBlob.size < 16 * 1024 * 1024) {
+        fetch(url, {
+          method: "PUT",
+          mode: "cors",
+          body: data,
+        }).then((res) => {
+          if (res.status === 200) {
+            return res.json();
+          }
+          throw new Error("File Failed to save");
+        })
+        .then((result) => {
+          alert(result["request"]);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+      }
+    }
+  }
 
   render() {
     return (
@@ -152,12 +193,26 @@ class DocViewer extends React.Component {
               </div>
             </Col>
             <Col xs={2}>
-              <div id="topBar"></div>
+              <button
+                id="save"
+                className={this.state.editable ? "backButton" : "hidden"}
+                onClick={this.handleFileSave}
+              >
+                Submit
+              </button>
             </Col>
           </Row>
           <Row>
             <Col xs={12}>
-              <div id="text"></div>
+              <div id="text" className="docViewerContainer">
+                <textarea
+                  id="textarea"
+                  className={this.state.editable ? "textArea" : "hidden"}
+                  spellCheck={this.state.code}
+                  value={this.state.value}
+                  onChange={this.handleFileChange}
+                ></textarea>
+              </div>
             </Col>
           </Row>
           {/* <Container fluid></Container> */}
