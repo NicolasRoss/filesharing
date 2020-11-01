02b9d8a72f78c712882d4e0cfd854902b763da18
Noah Nichols <noahjacknichols@gmail.com>
2020-10-31 21:19:44 -0400

    server actually works well

--- doc-share/src/components/api.jsx
+++ doc-share/src/components/api.jsx
@@ -1,9 +1,10 @@
 //for ec2 backend instance
-// export const API = "http://3.136.172.47:5000";
+// export const API = "https://declr.dev/api";
+// export const HOST = "https://declr.dev";
 
 //for local development
-export const API = "http://localhost:5000";
-// export const HOST = "http://3.136.172.47";
+export const API = "http://localhost:5000/api/";
+
 export const HOST = "http://localhost:3000";
 
 // export const API = "172.31.20.122:5000"
